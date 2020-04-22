8d01631cd97f0a694055408c5a44554fe9daa6e6
devAditya <aditya@cluelessmonk.com>
2020-04-22 19:13:52 +0530

    created a ripple effect animation

---
+++ lib/views/RippleAnimation.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class RippleAnimation extends StatefulWidget {
+  @override
+  _RippleAnimationState createState() => _RippleAnimationState();
+}
+
+class _RippleAnimationState extends State<RippleAnimation> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      lowerBound: 0.5,
+      duration: Duration(seconds: 3),
+    )..repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Title")),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return AnimatedBuilder(
+      animation: CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn),
+      builder: (context, child) {
+        return Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            _buildContainer(150 * _controller.value),
+            Align(
+              child: Container(
+                height: 70,
+                width: 70,
+                child: CircleAvatar(
+                  child: Icon(Icons.calendar_today),
+                  backgroundColor: Colors.white,
+                ),
+              ),
+            ),
+
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildContainer(double radius) {
+    return Container(
+      width: radius,
+      height: radius,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white.withOpacity(1 - _controller.value),
+      ),
+    );
+  }
+}
